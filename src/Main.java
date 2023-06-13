d65ade7a59c182b9578ae22d67988d31e41b13b7
rohanparikh <parikhrs@mail.uc.edu>
2023-06-12 20:46:47 -0400

    Basic Working Code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-       
+
+        double springCost = 100.5;
+        double summerCost = 240.8;
+        double fallCost = 145.5;
+        double winterCost = 325.2;
+        double totalYearlyCosts = 0.0;
+
+        totalYearlyCosts = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The Spring Home Maintenance Costs are $" + springCost);
+        System.out.println("The Summer Home Maintenance Costs are $" + summerCost);
+        System.out.println("The Fall Home Maintenance Costs are $" + fallCost);
+        System.out.println("The Winter Home Maintenance Costs are $" + winterCost);
+        System.out.println("The Combined Total Yearly Home Maintenance Costs for the Four Seasons are $" + totalYearlyCosts);
+
+
     }
 }
